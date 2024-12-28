f1a6109d04554f7f30ff2cc2b4428c0b80b62617
Kamil M <116840531+proxima812@users.noreply.github.com>
2024-12-28 13:23:22 +0500

    преукрасил группы, вид его. Чуть больше текст в постаз

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,16 @@
 import { defineCollection, z } from "astro:content";
 
 const groupsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    when: z.string(),
-    dateTime: z.string().default("20:00"),
-    body: z.string().optional(),
-    type: z.string().default("Онлайн"),
-    link: z.string().default("https://t.me/aiz_itta"),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      when: z.string(),
+      dateTime: z.string().default("20:00"),
+      body: z.string().optional(),
+      logo: image().optional(),
+      type: z.string().default("Онлайн"),
+      link: z.string().default("https://t.me/aiz_itta"),
+    }),
 });
 
 const diaryCollection = defineCollection({
