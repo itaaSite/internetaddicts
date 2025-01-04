992e2f6f000bb872ae86ce2f7f21d1e4f175a475
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-01-04 17:20:13 +0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,19 @@ import partytown from "@astrojs/partytown"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
-import keystatic from "@keystatic/astro"
-import { defineConfig } from "astro/config"
 
+import keystatic from "@keystatic/astro"
 import icon from "astro-icon"
+import { defineConfig } from "astro/config"
 
 export default defineConfig({
 	site: "https://internetaddicts.ru/",
 	prefetch: {
 		defaultStrategy: "viewport",
 	},
-
 	integrations: [
 		react(),
 		markdoc(),
-
 		keystatic(),
 		mdx(),
 		partytown({
