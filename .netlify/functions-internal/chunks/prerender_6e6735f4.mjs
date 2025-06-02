baa47c788c044d3ce049378101540a1af8bcfc2f
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-06-02 16:59:41 +0500

    3

--- .netlify/functions-internal/chunks/prerender_6e6735f4.mjs
+++
@@ -1,9 +0,0 @@
-const noop = () => {};
-export const $ = noop;
-export const _ = noop;
-export const a = noop;
-export const b = noop;
-export const g = noop;
-export const i = noop;
-export const p = noop;
-export const s = noop;
