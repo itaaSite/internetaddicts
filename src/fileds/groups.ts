fa6eeb5e9a3063323d5be90e883b9c325242820c
Kamil M <116840531+proxima812@users.noreply.github.com>
2025-06-04 14:01:15 +0500

    1

--- src/fileds/groups.ts
+++ src/fileds/groups.ts
@@ -17,23 +17,43 @@ export const groups = collection({
 			directory: "src/assets/images/groups_logos",
 			publicPath: "../../assets/images/groups_logos/",
 		}),
-		when: fields.text({
-			label: "Время",
-			defaultValue: "Пятница",
+		// when: fields.text({
+		// 	label: "Время",
+		// 	defaultValue: "Пятница",
+		// 	description:
+		// 		"Когда? Понедельник, Вторник, Среда, Четверг, Пятница, Суббота или Воскресенье",
+		// }),
+		when: fields.select({
+			label: "Когда проводиться группа?",
 			description:
 				"Когда? Понедельник, Вторник, Среда, Четверг, Пятница, Суббота или Воскресенье",
+			options: [
+				{ label: "Понедельник", value: "Понедельник" },
+				{ label: "Вторник", value: "Вторник" },
+				{ label: "Среда", value: "Среда" },
+				{ label: "Четверг", value: "Четверг" },
+				{ label: "Пятница", value: "Пятница" },
+				{ label: "Суббота", value: "Суббота" },
+				{ label: "Воскресенье", value: "Воскресенье" },
+			],
+			defaultValue: "Понедельник",
 		}),
 		dateTime: fields.text({
 			label: "Время",
 			description: "Время проведения собрания по МСК +3",
 		}),
-		type: fields.text({
+		type: fields.select({
 			label: "Тип группы",
 			description: '"Онлайн" либо "Живая"',
+			options: [
+				{ label: "Онлайн", value: "Онлайн" },
+				{ label: "Живая", value: "Живая" },
+			],
 			defaultValue: "Онлайн",
 		}),
 		link: fields.url({
 			label: "Ссылка (URL) на нее, либо карта (если живая группа)",
+			description: "https://t.me/aiz_meetings",
 		}),
 	},
 })
